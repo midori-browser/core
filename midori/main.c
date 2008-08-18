41bd88efca993cf92240d4d1d5fb46da2630bc3b
Christian Dywan <christian@twotoasts.de>
2008-08-18 23:43:15 +0200

    Capitalize Midori when bailing out on an error

--- midori/main.c
+++ midori/main.c
@@ -409,9 +409,9 @@ main (int argc,
                              GETTEXT_PACKAGE, &error))
     {
         if (error->code == G_OPTION_ERROR_UNKNOWN_OPTION)
-            g_print ("%s - %s\n", _("midori"), _("Unknown argument."));
+            g_print ("%s - %s\n", _("Midori"), _("Unknown argument."));
         else
-            g_print ("%s - %s", _("midori"), _("Failed to setup interface."));
+            g_print ("%s - %s", _("Midori"), _("Failed to setup interface."));
         g_error_free (error);
         return 1;
     }
