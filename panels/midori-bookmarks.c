0f157153e764d3c014d6df423df9a8c30de8a5ee
Alexander Butenko <a.butenka@gmail.com>
2010-06-16 20:57:52 +0200

    Order bookmarks descending. Folders should always be on top

--- panels/midori-bookmarks.c
+++ panels/midori-bookmarks.c
@@ -139,7 +139,7 @@ midori_bookmarks_read_from_db (MidoriBookmarks* bookmarks,
     db = g_object_get_data (G_OBJECT (bookmarks->array), "db");
 
     sqlcmd = "SELECT uri, title, type from bookmarks where folder = ?"
-             " ORDER BY type, title ASC";
+             " ORDER BY type DESC";
     result = sqlite3_prepare_v2 (db, sqlcmd, -1, &statement, NULL);
     sqlite3_bind_text (statement, 1, g_strdup(folder), -1, g_free);
 
