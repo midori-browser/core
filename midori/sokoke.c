d83b9619e1c1eb7dfed39cae20cd8ca68cf2ccc0
Paweł Forysiuk <tuxator@o2.pl>
2010-10-21 21:09:01 +0200

    Don't regard about: URIs as external

--- midori/sokoke.c
+++ midori/sokoke.c
@@ -745,7 +745,8 @@ sokoke_external_uri (const gchar* uri)
     gchar* scheme;
     GAppInfo* info;
 
-    if (!uri || !strncmp (uri, "http", 4))
+    if (!uri || !strncmp (uri, "http", 4)
+             || !strncmp (uri, "about:", 6))
         return FALSE;
 
     scheme = g_uri_parse_scheme (uri);
