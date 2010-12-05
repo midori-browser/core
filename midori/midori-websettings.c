a8cc977a7c960d2bab5c4cbeec8715be4fcb9d42
Christian Dywan <christian@twotoasts.de>
2010-12-05 23:05:22 +0100

    Omit Sidepanel button from default toolbar
    It is in the compact menu, and otherwise menubar.

--- midori/midori-websettings.c
+++ midori/midori-websettings.c
@@ -547,7 +547,7 @@ midori_web_settings_class_init (MidoriWebSettingsClass* class)
                                      "toolbar-items",
                                      _("Toolbar Items"),
                                      _("The items to show on the toolbar"),
-                                     "TabNew,Back,Forward,Next,ReloadStop,Location,Panel,Search,Trash",
+                                     "TabNew,Back,Forward,Next,ReloadStop,Location,Search,Trash",
                                      flags));
 
     g_object_class_install_property (gobject_class,
