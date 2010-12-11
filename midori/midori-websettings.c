3326c554b60a749f8de3aaaf04a2b77ab5ad9ae1
Christian Dywan <christian@twotoasts.de>
2010-12-11 20:12:54 +0100

    Put Add Bookmark instead of Homepage in the toolbar

--- midori/midori-websettings.c
+++ midori/midori-websettings.c
@@ -547,7 +547,7 @@ midori_web_settings_class_init (MidoriWebSettingsClass* class)
                                      "toolbar-items",
                                      _("Toolbar Items"),
                                      _("The items to show on the toolbar"),
-                                     "TabNew,Back,Forward,Next,ReloadStop,Homepage,Location,Search,Trash",
+                                     "TabNew,Back,Forward,Next,ReloadStop,BookmarkAdd,Location,Search,Trash",
                                      flags));
 
     g_object_class_install_property (gobject_class,
