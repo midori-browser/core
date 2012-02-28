5a74f5baafd565254645478202c1f52d3c24dc63
Christian Dywan <christian@twotoasts.de>
2012-02-29 00:54:03 +0100

    Reverse packing of statusbar features bbox
    Apparently with GTK+3 this makes a difference.

--- extensions/statusbar-features.c
+++ extensions/statusbar-features.c
@@ -170,7 +170,7 @@ statusbar_features_app_add_browser_cb (MidoriApp*       app,
         G_CALLBACK (statusbar_features_browser_notify_tab_cb), button);
     statusbar_features_browser_notify_tab_cb (browser, NULL, button);
     gtk_widget_show_all (bbox);
-    gtk_box_pack_start (GTK_BOX (statusbar), bbox, FALSE, FALSE, 3);
+    gtk_box_pack_end (GTK_BOX (statusbar), bbox, FALSE, FALSE, 3);
     g_object_unref (statusbar);
 
     g_signal_connect (extension, "deactivate",
