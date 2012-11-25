8338a3c2463f426ab390793b849e172029c6ccb8
Christian Dywan <christian@twotoasts.de>
2012-11-25 17:43:57 +0100

    Add potfiles unit test and update POTFILES.in

--- tests/potfiles.sh
+++ tests/potfiles.sh
@@ -5,9 +5,11 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-echo Running 'licensecheck'
-test -z $(which licensecheck) && echo ...SKIPPED: not installed && return 0
+echo Checking POTFILES.in for completeness
 test -n "$SRCDIR" && cd $SRCDIR
 test -z "$BLDDIR" && BLDDIR=_build
-find . \! -path './.*/*' -a \! -path "./$BLDDIR/*" -a \! -path "./debian/*" | xargs licensecheck | grep UNKNOWN && exit 1
-echo ...OK
+for i in $(find . -regextype posix-egrep \! -regex "./($BLDDIR|debian|tests)/.+" -a -regex './[^.]+.+[.](vala|c)'); do
+    grep -q $(basename $i) po/POTFILES.in || FILES="$FILES$i\n"
+done
+test -n "$FILES" && echo "$FILES...FAILED"
+test -z "$FILES" && echo "...OK"
