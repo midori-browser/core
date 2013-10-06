6c3fd23e50dbd824f14c9ccd51a1cb2267cda821
gue5t <gue5t@midori.launchpad>
2013-10-06 11:41:01 -0500

    only autofocus error retry button in main frames

--- midori/midori-view.c
+++ midori/midori-view.c
@@ -1206,6 +1206,13 @@ midori_view_display_error (MidoriView*     view,
         const gchar* icon;
         gchar* favicon;
         gchar* result;
+        gboolean is_main_frame;
+
+        #ifdef HAVE_WEBKIT2
+        is_main_frame = TRUE;
+        #else
+        is_main_frame = web_frame && (webkit_web_view_get_main_frame(view->web_view) == web_frame);
+        #endif
 
         #if !GTK_CHECK_VERSION (3, 0, 0)
         /* g_object_get_valist: object class `GtkSettings' has no property named `gtk-button-images' */
@@ -1233,6 +1240,7 @@ midori_view_display_error (MidoriView*     view,
             "{tryagain}", try_again,
             "{uri}", uri,
             "{hide-button-images}", show_button_images ? "" : "display:none",
+            "{autofocus}", is_main_frame ? "autofocus=\"true\" " : "",
             NULL);
         g_free (favicon);
         g_free (title_escaped);
@@ -1263,6 +1271,10 @@ webkit_web_view_load_error_cb (WebKitWebView*  web_view,
 {
     #ifdef HAVE_WEBKIT2
     void* web_frame = NULL;
+    /*in WebKit2's UIProcess/API/gtk/WebKitLoaderClient.cpp,
+    didFailProvisionalLoadWithErrorForFrame early-returns if the frame isn't
+    main, so we know that the pertinent frame here is the view's main frame--so
+    it's safe for midori_view_display_error to assume it fills in a main frame*/
     #endif
     gchar* title;
     gchar* message;
