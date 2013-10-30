e2ee472e04eb55cb452bdb51f96b06ec2fd936f5
Christian Dywan <christian.dywan@canonical.com>
2013-10-30 01:09:53 +0100

    Don't shadow filename in midori_settings_save_to_file

--- midori/midori-websettings.c
+++ midori/midori-websettings.c
@@ -1587,12 +1587,12 @@ midori_settings_save_to_file (MidoriWebSettings* settings,
             KATZE_ARRAY_FOREACH_ITEM (extension, extensions)
                 if (midori_extension_is_active (extension))
                 {
-                    const gchar* filename = g_object_get_data (G_OBJECT (extension), "filename");
-                    g_return_val_if_fail (filename != NULL, FALSE);
-                    if (filename && strchr (filename, '/'))
-                        g_warning ("%s: %s unexpected /", G_STRFUNC, filename);
+                    const gchar* extension_filename = g_object_get_data (G_OBJECT (extension), "filename");
+                    g_return_val_if_fail (extension_filename != NULL, FALSE);
+                    if (extension_filename && strchr (extension_filename, '/'))
+                        g_warning ("%s: %s unexpected /", G_STRFUNC, extension_filename);
                     gchar* key = katze_object_get_string (extension, "key");
-                    gchar* subname = key ? g_strdup_printf ("%s/%s", filename, key) : g_strdup (filename);
+                    gchar* subname = key ? g_strdup_printf ("%s/%s", extension_filename, key) : g_strdup (extension_filename);
                     g_key_file_set_boolean (key_file, "extensions", subname, TRUE);
                     g_free (key);
                     g_free (subname);
