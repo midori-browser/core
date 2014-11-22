db98a1ec6d1abff54e76f7de35fffff8497fed0b
Christian Dywan <christian.dywan@canonical.com>
2014-11-22 01:07:10 +0100

    Add proper copyright headers to element_hider and autosuggestcontrol

--- data/adblock/element_hider.js
+++ data/adblock/element_hider.js
@@ -1,3 +1,13 @@
+/*
+ Copyright (C) 2014 Alexander V. Butenko <a.butenka@gmail.com>
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ See the file COPYING for the full license text.
+ */
 function getElementsByAttribute (strTagName, strAttributeName, arrAttributeValue) {
     var arrElements = document.getElementsByTagName (strTagName);
     var arrReturnElements = new Array();
