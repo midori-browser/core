4ac211028ecae1d6897b9d0f4a0f6909aaaabfa0
Christian Dywan <christian.dywan@canonical.com>
2015-07-11 12:18:11 -0400

    Add copright header to sanitize_bar.sh

--- tools/sanitize_bzr.sh
+++ tools/sanitize_bzr.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Copyright (C) 2015 Paweł Forysiuk <tuxator@o2.pl>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# See the file COPYING for the full license text.
+
 BZR_PLUGINS_DIR=${HOME}/.bazaar/plugins/
 if [ ! -d $BZR_PLUGINS_DIR ]; then
     mkdir -p $BZR_PLUGINS_DIR
