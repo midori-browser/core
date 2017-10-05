0c4d3b94b85305a005e97a77c31464b709729676
Christian Dywan <christian@twotoasts.de>
2017-10-06 01:13:58 +0200

    Disable Zeitgeist and check error != NULL

--- midori/midori-view.c
+++ midori/midori-view.c
@@ -446,8 +446,10 @@ midori_view_apply_icon (MidoriView*  view,
             pixbuf = gtk_icon_info_load_icon (icon_info, &error);
         g_strfreev (icon_names);
         if (pixbuf == NULL) {
-            g_warning ("Could not load pixbuf for icon '%s': %s\n", icon_name, error->message);
-            g_clear_error (&error);
+            if (error != NULL) {
+                g_warning ("Could not load pixbuf for icon '%s': %s\n", icon_name, error->message);
+                g_clear_error (&error);
+            }
 
             /* view->icon cannot be set to NULL, so we simply leave it as-is */
             return;
