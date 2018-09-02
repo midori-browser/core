89e02daaa6ba30fa8c26e8dfc571b85402531769
Christian Dywan <christian@twotoasts.de>
2018-09-02 12:33:56 +0200

    Make POTFILES.in check ignore parts folder
    Otherwise building the snap fails the test.

--- tests/potfiles.sh
+++ tests/potfiles.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
-# Copyright 2012 Christian Dywan <christian@twotoasts.de>
+# Copyright 2012-2018 Christian Dywan <christian@twotoasts.de>
 #
 # This script is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -8,7 +8,7 @@
 echo Checking POTFILES.in for completeness
 test -n "$SRCDIR" && cd $SRCDIR
 test -z "$BLDDIR" && BLDDIR=_build
-for i in $(find . -regextype posix-egrep \! -regex "./($BLDDIR|_.+|debian|tests)/.+" -a -regex './[^.]+.+[.](vala|c)'); do
+for i in $(find . -regextype posix-egrep \! -regex "./($BLDDIR|_.+|debian|tests|parts)/.+" -a -regex './[^.]+.+[.](vala|c)'); do
     grep -q $(basename $i) po/POTFILES.in || FILES="$FILES$i\n"
 done
 test -n "$FILES" && echo "$FILES...FAILED"
