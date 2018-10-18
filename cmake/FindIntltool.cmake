8aed3a418ec668d1ff2be8edbf103d57aaeaa8e9
Christian Dywan <christian@twotoasts.de>
2018-10-19 00:51:15 +0200

    Merge translations into plugin descriptions (#104)
    Plugin descriptions are basically `.desktop` files and Peas supports picking up locale-specific keys suffixed with `[language]` which means keys prefixed with an underscore in the `.plugin.in` can be extracted.

--- cmake/FindIntltool.cmake
+++ cmake/FindIntltool.cmake
@@ -5,16 +5,16 @@
 # Copyright (C) 2013-2018 Christian Dywan
 
 find_program (INTLTOOL_MERGE_EXECUTABLE intltool-merge)
-find_program (INTLTOOL_UPDATE_EXECUTABLE intltool-update)
-
 if (INTLTOOL_MERGE_EXECUTABLE)
-    set (INTLTOOL_MERGE_FOUND TRUE)
-    macro (INTLTOOL_MERGE_DESKTOP desktop_id po_dir)
-        add_custom_target (${desktop_id}.desktop ALL
+    macro (INTLTOOL_MERGE_DESKTOP_LIKE target po_dir)
+        add_custom_target (${target} ALL
             COMMAND ${INTLTOOL_MERGE_EXECUTABLE} --desktop-style ${CMAKE_SOURCE_DIR}/${po_dir}
-                ${CMAKE_CURRENT_SOURCE_DIR}/${desktop_id}.desktop.in ${desktop_id}.desktop
-            DEPENDS ${desktop_id}.desktop.in
+                ${CMAKE_CURRENT_SOURCE_DIR}/${target}.in ${target}
+            DEPENDS ${target}.in
         )
+    endmacro (INTLTOOL_MERGE_DESKTOP_LIKE target po_dir)
+    macro (INTLTOOL_MERGE_DESKTOP desktop_id po_dir)
+        INTLTOOL_MERGE_DESKTOP_LIKE ("${desktop_id}.desktop" ${po_dir})
         install (FILES "${CMAKE_CURRENT_BINARY_DIR}/${desktop_id}.desktop"
                  DESTINATION "${CMAKE_INSTALL_PREFIX}/share/applications")
     endmacro (INTLTOOL_MERGE_DESKTOP desktop_id po_dir)
@@ -27,14 +27,16 @@ if (INTLTOOL_MERGE_EXECUTABLE)
         install (FILES "${CMAKE_CURRENT_BINARY_DIR}/${desktop_id}.appdata.xml"
                  DESTINATION "${CMAKE_INSTALL_PREFIX}/share/appdata")
     endmacro (INTLTOOL_MERGE_APPDATA desktop_id po_dir)
+else ()
+    message(FATAL_ERROR "intltool-merge not found")
 endif ()
 
+find_program (INTLTOOL_UPDATE_EXECUTABLE intltool-update)
 if (INTLTOOL_UPDATE_EXECUTABLE)
-    set (INTLTOOL_UPDATE_FOUND TRUE)
     add_custom_target (pot
         COMMAND ${INTLTOOL_UPDATE_EXECUTABLE} "-p" "-g" ${GETTEXT_PACKAGE}
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/po"
         )
+else ()
+    message(FATAL_ERROR "intltool-update not found")
 endif ()
-
-
