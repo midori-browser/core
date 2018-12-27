d3ff0385683ee6547363b61a18451d2e3a7720a8
Christian Dywan <christian@twotoasts.de>
2018-12-27 01:04:11 +0100

    android: save last visited website
    The URL will be loaded next time the browser is opened.

--- app/src/main/java/org/midori_browser/midori/WebViewClient.kt
+++ app/src/main/java/org/midori_browser/midori/WebViewClient.kt
@@ -1,5 +1,6 @@
 package org.midori_browser.midori
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.webkit.WebView
@@ -16,5 +17,12 @@ class WebViewClient(val activity: BrowserActivity) : WebViewClient() {
 
         activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
         return true
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        val editor = activity.getSharedPreferences("config", Context.MODE_PRIVATE).edit()
+        editor.putString("openTabs", url)
+        editor.apply()
+        super.onPageFinished(view, url)
     }
 }
